7c1bd37cabd4de5ef29716a69fc37e85337cc7be
jirawat <jirawatball@hotmail.com>
2014-11-10 11:52:26 +0700

    Add Content2

---
+++ Assets/Ball/Script/Control/PlayerProfile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProfile : MonoBehaviour {
+
+	public int Hp;
+	public bool TurnOn;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Ball/Script/Object/WaterTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterTrigger : MonoBehaviour {
+
+	void OnTriggerStay(Collider other) {
+		if (other.tag == "Player"){
+			other.GetComponent<PlayerProfile>();
+		}
+	}
+}
